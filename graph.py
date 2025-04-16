43a3a9d691021fe835ced98d430bb24fef727a60
foodshop1 <[e](mailto:your@email.com)lroyquan@gmail.com>
2025-04-16 11:22:29 -0400

    Updated project with new code

--- graph.py
+++ graph.py
@@ -4,6 +4,8 @@ matplotlib.use('Agg')  # Non-interactive backend for script-only usage
 from nhl_data import stats
 import matplotlib.pyplot as plt
 import os
+from matplotlib.patches import Patch
+
 
 json_data = stats()
 
@@ -12,14 +14,22 @@ def plot_nhl_points(json_data):
     team_data.sort(key=lambda x: x[1], reverse=True)
 
     team_names, points = zip(*team_data)
+    colors = ['green' if i < 16 else 'orange' for i in range(len(team_names))]
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.bar(team_names, points, color='skyblue')
+    plt.plot()
+    ax.bar(team_names, points, color=colors)
     ax.set_title('NHL Teams by Points')
     ax.set_xlabel('Teams')
     ax.set_ylabel('Points')
     plt.xticks(rotation=90)
     plt.tight_layout()
+    
+    legend_elements = [
+        Patch(facecolor='green', label='Playoff Spot'),
+        Patch(facecolor='orange', label='Non-Playoff')
+    ]
+    ax.legend(handles=legend_elements, loc='upper right')
 
     return fig
 
